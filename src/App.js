679e3788065287413aab78762d391687f8260517
Cengiz YILMAZ <terabilgi@gmail.com>
2022-05-28 01:56:19 +0300

    props.children kullanıldı

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
+import HelloWorld from "./components/HelloWorld"
 
 function App() {
   return (
-    <div className="App">
-
+    <div className='App'>
+      <HelloWorld butonad='Tıkla beni' resim='https://i.ytimg.com/vi/JAvOPQOMCzk/hqdefault.jpg'>
+        <img
+          src='https://blogs.cdn.medel.com/blog.medel.com/uploads/20201125131016/ses.jpg'
+          alt='Merhaba' width="30%"
+        />
+        <h2>Alt  children Öğe</h2>
+      </HelloWorld>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/components/HelloWorld.js
@@ -0,0 +1,20 @@
+import React  from "react";
+
+
+const HelloWorld = (props) => {
+     
+    return (
+      <div>
+        <p>Merhaba</p>
+        <br />
+        <button>{props.butonad}</button>
+        <br />
+        <br />
+        <span>{props.children}</span>
+        <img src={props.resim} alt='' />
+      </div>
+    )
+
+}
+
+export default HelloWorld;
